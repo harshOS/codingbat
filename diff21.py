e60e05809ee4cd51a5458a9f8aaf0c8778373d05
harshOS <harshithngr@gmail.com>
2020-02-21 18:07:03 +0530

    final

---
+++ diff21.py
@@ -0,0 +1,11 @@
+# Warmup-1 > diff21
+# problem 2
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+def diff21(n):
+  if n > 21:
+    return 2*(n-21)
+  else:
+    return 21 - n
+
+print(diff21(19))
