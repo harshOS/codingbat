f15c0e15bb0fa1f115b389cae83e484a52a5863e
harshOS <harshithngr@gmail.com>
2020-02-21 21:11:42 +0530

    last commit

--- sleep_in.py
+++ sleep_in.py
@@ -1,5 +1,5 @@
 ##Warmup-1 sleep_in
-##problem statement
+##problem statement from coding bat
 ##The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
 def sleep_in(weekday, vacation):
