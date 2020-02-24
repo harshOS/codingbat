52e9f7ada3d200555e8049060f7a92dba443f404
harshOS <harshithngr@gmail.com>
2020-02-24 20:05:11 +0530

    final

---
+++ not_string.py
@@ -0,0 +1,8 @@
+#problem
+#Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+def not_string(str):
+  if str[0:3] == 'not':
+    return str
+  else:
+    return 'not '+str
