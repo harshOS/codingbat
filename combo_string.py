2adbe9df372dd2ba297d73218f224aa745d8a0ce
harshOS <harshithngr@gmail.com>
2020-02-27 20:36:36 +0530

    8th day

---
+++ combo_string.py
@@ -0,0 +1,18 @@
+#problem link - https://codingbat.com/prob/p194053
+# Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+#
+#
+# combo_string('Hello', 'hi') → 'hiHellohi'
+# combo_string('hi', 'Hello') → 'hiHellohi'
+# combo_string('aaa', 'b') → 'baaab'
+
+def combo_string(a, b):
+  if len(a) < len(b):
+    short = a
+    long1 = b
+  else:
+    short = b
+    long1 = a
+  return short+long1+short
+
+print(combo_string('aaa', 'b'))
