f2c41a3166f1a0c96f85be579ffd8377bcfd1e70
harshOS <harshithngr@gmail.com>
2020-03-06 22:57:02 +0530

    List problems

---
+++ rotate_left3.py
@@ -0,0 +1,13 @@
+# problem link -https://codingbat.com/prob/p148661
+# Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+#
+# rotate_left3([1, 2, 3]) → [2, 3, 1]
+# rotate_left3([5, 11, 9]) → [11, 9, 5]
+# rotate_left3([7, 0, 0]) → [0, 0, 7]
+
+def rotate_left3(nums):
+  nums.append(nums[0])
+  del nums[0]
+  return nums
+
+print(rotate_left3([7, 0, 0]))
