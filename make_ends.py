503e440560e75d28aa6c424ee22c5b9e0847cb2c
Harshithkumar NG <59756315+harshOS@users.noreply.github.com>
2020-03-09 23:34:39 +0530

    Update link

--- make_ends.py
+++ make_ends.py
@@ -1,5 +1,5 @@
 # Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
-#
+# problem link -
 #
 # make_ends([1, 2, 3]) → [1, 3]
 # make_ends([1, 2, 3, 4]) → [1, 4]
